31873cd4a773e4d991841a0833812743fb506443
Francesco Bonacci <Francesco Bonacci>
2017-02-04 23:53:26 +0100

    Add Module 2 Material

---
+++ M2 - Xamarin.Android Fondamenti di Android/Demo 2/TipCalculator/TipCalculator/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace TipCalculator
+{
+    [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private EditText inputBill;
+        private Button calculateButton;
+        private TextView outputTip;
+        private TextView outputTotal;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += OnCalculateClick;
+        }
+
+        protected void OnCalculateClick(object sender, EventArgs e)
+        {
+            string text = inputBill.Text;
+
+            double bill;
+            if (double.TryParse(text, out bill))
+            {
+                var tip = bill * 0.15;
+                var total = bill + tip;
+
+                outputTip.Text = tip.ToString("C");
+                outputTotal.Text = total.ToString("C");
+            }
+        }
+    }
+}
+
