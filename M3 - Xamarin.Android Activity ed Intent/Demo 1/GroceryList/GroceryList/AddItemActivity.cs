86747e8c18917ada780a4cdd0f205998a00720be
Francesco Bonacci <Francesco Bonacci>
2017-02-12 18:32:33 +0100

    Add M3 Module - Demo 1

---
+++ M3 - Xamarin.Android Activity ed Intent/Demo 1/GroceryList/GroceryList/AddItemActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace GroceryList
+{
+    [Activity(Label = "Add Item")]
+    public class AddItemActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.AddItem);
+
+            FindViewById<Button>(Resource.Id.saveButton).Click += OnSaveClick;
+        }
+
+        void OnSaveClick(object sender, EventArgs e)
+        {
+            string name = FindViewById<EditText>(Resource.Id.nameInput).Text;
+            int count = int.Parse(FindViewById<EditText>(Resource.Id.countInput).Text);
+        }
+    }
+}
---
+++ M3 - Xamarin.Android Activity ed Intent/Demo 1/GroceryList/GroceryList/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace GroceryList
+{
+    [Activity(Label = "Grocery List", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        public static List<Item> Items = new List<Item>();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            Items.Add(new Item("Milk", 2));
+            Items.Add(new Item("Crackers", 1));
+            Items.Add(new Item("Apples", 5));
+
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            FindViewById<Button>(Resource.Id.itemsButton).Click += OnItemsClick;
+            FindViewById<Button>(Resource.Id.addItemButton).Click += OnAddItemClick;
+            FindViewById<Button>(Resource.Id.aboutButton).Click += OnAboutClick;
+        }
+
+        void OnItemsClick(object sender, EventArgs e)
+        {
+            // TODO
+            var intent = new Intent(this, typeof(ItemsActivity));
+            StartActivity(intent);
+        }
+
+        void OnAddItemClick(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(AddItemActivity));
+
+            StartActivity(intent);
+        }
+
+        void OnAboutClick(object sender, EventArgs e)
+        {
+            // TODO
+            StartActivity(typeof(AboutActivity));
+        }
+    }
+}
