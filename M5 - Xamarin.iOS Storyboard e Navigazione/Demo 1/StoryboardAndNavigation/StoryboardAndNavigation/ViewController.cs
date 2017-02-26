7b2c598463b54a7db7e5467af3cc8da1b890006c
francedot <francibon93@gmail.com>
2017-02-26 23:09:32 +0100

    Add Module M5 - Demo 1

---
+++ M5 - Xamarin.iOS Storyboard e Navigazione/Demo 1/StoryboardAndNavigation/StoryboardAndNavigation/ViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using UIKit;
+
+namespace StoryboardAndNavigation
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		partial void SwitchNight_ValueChanged(UISwitch sender)
+		{
+			if (sender.On)
+				this.View.BackgroundColor = UIColor.FromRGB(25, 25, 112);
+			else
+				this.View.BackgroundColor = UIColor.White;
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
