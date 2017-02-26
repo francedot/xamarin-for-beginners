0bf8cc43079aeddf61dc6ad19f8646c412275a4b
francedot <francibon93@gmail.com>
2017-02-26 10:19:12 +0100

    Add M4 Module - Demo 1

---
+++ M4 - Xamarin.iOS Fondamenti di iOS/Demo 1/TipCalculator/TipCalculator/ViewController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace TipCalculator
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController()
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			View.BackgroundColor = UIColor.White;
+
+			UITextField totalAmount = new UITextField(new CGRect(20, 28, View.Bounds.Width - 40, 35))
+			{
+				KeyboardType = UIKeyboardType.DecimalPad,
+				BorderStyle = UITextBorderStyle.RoundedRect,
+				Placeholder = "Enter Total Amount"
+			};
+
+			UIButton calcButton = new UIButton(UIButtonType.Custom)
+			{
+				Frame = new CGRect(20, 71, View.Bounds.Width - 40, 45),
+				BackgroundColor = UIColor.FromRGB(0.2f, 0.5f, 0.4f),
+			};
+			calcButton.SetTitle("Calculate", UIControlState.Normal);
+
+			UILabel resultLabel = new UILabel(new CGRect(20, 124, View.Bounds.Width - 40, 40))
+			{
+				TextAlignment = UITextAlignment.Center,
+				Font = UIFont.SystemFontOfSize(24),
+				Text = "Tip is $0.00"
+			};
+
+			View.AddSubviews(totalAmount, calcButton, resultLabel);
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
