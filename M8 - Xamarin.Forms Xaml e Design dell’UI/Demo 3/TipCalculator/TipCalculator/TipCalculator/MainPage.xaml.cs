effbe6bdbe90ba74a3e710ee280e51eb9a393408
Francesco Bonacci <Francesco Bonacci>
2017-04-11 17:19:49 +0200

    Update Module M8 - Add Demo 3

---
+++ M8 - Xamarin.Forms Xaml e Design dell’UI/Demo 3/TipCalculator/TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButtonOnClicked(object sender, EventArgs eventArgs)
+        {
+            string text = billEntry.Text;
+
+            double bill;
+            if (double.TryParse(text, out bill))
+            {
+                var tip = bill * 0.15;
+                var total = bill + tip;
+
+                tipLabel.Text = tip.ToString("C");
+                totalLabel.Text = total.ToString("C");
+            }
+        }
+    }
+}
