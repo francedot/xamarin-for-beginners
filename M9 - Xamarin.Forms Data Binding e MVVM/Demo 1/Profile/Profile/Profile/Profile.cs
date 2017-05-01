b347d7476b0bda6b2f74ece5dc0d80a6f888aad5
francedot <francibon93@gmail.com>
2017-05-01 17:11:37 +0200

    Add Module M9 - Demo 1

---
+++ M9 - Xamarin.Forms Data Binding e MVVM/Demo 1/Profile/Profile/Profile/Profile.cs
@@ -0,0 +1,14 @@
+﻿namespace Profile
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string BackgroundImage { get; set; }
+        public string ProfileImage { get; set; }
+        public string Tag { get; set; }
+        public string Description { get; set; }
+        public uint Likes { get; set; }
+        public uint Following { get; set; }
+        public uint Followers { get; set; }
+    }
+}
