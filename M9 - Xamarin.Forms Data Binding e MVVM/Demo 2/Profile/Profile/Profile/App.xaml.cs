c8547d68bf064010a5b46cff6f5b8f7557b3da6f
francedot <francibon93@gmail.com>
2017-05-01 17:24:13 +0200

    Add Module M9 - Demo 2

---
+++ M9 - Xamarin.Forms Data Binding e MVVM/Demo 2/Profile/Profile/Profile/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Profile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var model = new Profile
+            {
+                Name = "Francesco",
+                BackgroundImage = "",
+                ProfileImage = "https://avatars0.githubusercontent.com/u/11706033?v=3&s=400",
+                Description = "Jogger and TV series addicted. In spare time enjoy turning coffee into XAML while listening to good music.",
+                Tag = "Xamarin Developer",
+                Likes = 47,
+                Following = 900,
+                Followers = 957
+            };
+
+            Resources["myProfile"] = model;
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ M9 - Xamarin.Forms Data Binding e MVVM/Demo 2/Profile/Profile/Profile/NoSourceToPlaceholderConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Profile
+{
+    public class NoSourceToPlaceholderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text = value as string;
+            return string.IsNullOrEmpty(text) ? "Assets/placeholder.jpg" : string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
