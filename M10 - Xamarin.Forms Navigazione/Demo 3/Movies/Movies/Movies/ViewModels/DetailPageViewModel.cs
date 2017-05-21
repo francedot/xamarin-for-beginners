afe0882f41c5cfab183b84c33be8d287238e3af3
francedot <francibon93@gmail.com>
2017-05-21 22:33:19 +0200

    Add M10 - Demo 3

---
+++ M10 - Xamarin.Forms Navigazione/Demo 3/Movies/Movies/Movies/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Movies.Models;
+using Xamarin.Forms;
+
+namespace Movies.ViewModels
+{
+    public class DetailPageViewModel : ViewModelBase
+    {
+        public event EventHandler<EventArgs> GenreChanged; 
+
+        private IList<Movie> _movies;
+
+        public DetailPageViewModel()
+        {
+        }
+
+        public IList<Movie> Movies
+        {
+            get => _movies;
+            set
+            {
+                Set(ref _movies, value);
+                if (value != null)
+                {
+                    GenreChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
---
+++ M10 - Xamarin.Forms Navigazione/Demo 3/Movies/Movies/Movies/Views/DetailPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Movies.ViewModels;
+using Xamarin.Forms;
+
+namespace Movies.Views
+{
+    public partial class DetailPage : CarouselPage
+    {
+        public DetailPageViewModel ViewModel => this.BindingContext as DetailPageViewModel;
+
+        public DetailPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (ViewModel != null)
+            {
+                ViewModel.GenreChanged += OnGenreChanged;
+            }
+        }
+
+        private void OnGenreChanged(object sender, EventArgs eventArgs)
+        {
+            this.Children.Clear();
+
+            foreach (var movie in ViewModel.Movies)
+            {
+                this.Children.Add(new MoviePage()
+                {
+                    BindingContext = movie
+                });
+            }
+        }
+    }
+}
